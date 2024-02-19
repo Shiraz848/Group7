aa1aa7b8338ee278bb15cb484d3e66ff0c37cf01
Shiraz <shiraz848@gmail.com>
2024-02-19 20:15:28 +0200

    sign in final!!!!!!!

--- signIn.js
+++ signIn.js
@@ -2,33 +2,37 @@ document.addEventListener("DOMContentLoaded", () => {
     const signInForm = document.querySelector(".form");
 
     signInForm.addEventListener("submit", (event) => {
-        event.preventDefault(); // Prevent the form from submitting normally
+        event.preventDefault();
 
         // Get user input
         const email = document.querySelector('input[name="email"]').value.trim();
         const password = document.querySelector('input[name="password"]').value.trim();
 
-        // Retrieve registered users from local storage
+        // get registered users from local storage
         const registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
 
-        // Find user with matching credentials
+        // Find user with matching information
         const user = registeredUsers.find(user => user.email === email && user.password === password);
 
         if (user) {
-            // Store signed-in user data in local storage
+            // Store user data in local storage
             localStorage.setItem('signedInUser', JSON.stringify(user));
-            // Redirect to the Find Coach page if user is found
+            // if user is found go to the Find Coach page
             window.location.href = "findCoach.html";
         } else {
-            // Display error message if user is not found
-            displayError("Invalid email or password." + "Please try again.");
+            // Display error message
+            displayError("Invalid email or password.");
         }
     });
 });
 
 // Function to display error message
 const displayError = (message) => {
-    const errorElement = document.querySelector('.msg');
-    errorElement.textContent = message;
-    errorElement.style.display = 'block'; // Show the error message
+    const msg = document.querySelector('.msg');
+    msg.innerHTML = message;
+    msg.classList.add('error');
+    setTimeout(() => {
+        msg.innerHTML = '';
+        msg.classList.remove('error');
+    }, 5000);
 };
