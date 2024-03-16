6313c2127fd6fc3b89e3abe8b82d53e75a55f9a8
Shiraz <shiraz848@gmail.com>
2024-03-16 10:45:06 +0200

    coach details working friday upd

--- WEB_partC/components/coachDetails/coachDetails.py
+++ WEB_partC/components/coachDetails/coachDetails.py
@@ -8,16 +8,3 @@ coachDetails_bp = Blueprint(
     static_url_path='/coachDetails',
     template_folder='templates'
 )
-
-
-# @coachDetails_bp.route('/findCoach/coach/<string:phone>')
-# def coach_details(phone):
-#     print("coach_details route was called.")
-#     # phone = request.args['phone']
-#     coach = coaches_col.find_one({'phone': phone})
-#     if coach:
-#         # The template_name_or_list parameter is not typically used like this,
-#         # so just render_template('coachDetails.html', coach=coach) should suffice.
-#         return render_template('findCoach.html', coach=coach, show_details=True)
-#     else:
-#         return "Coach not found", 404
