3b67f8edad3c3ca5e6159b3b7323f2bf7275a971
Shiraz <shiraz848@gmail.com>
2024-03-22 13:49:48 +0200

    base upd 22-3

---
+++ WEB_partC/static/js/base.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const flashMessagesContainer = document.getElementById('flash-messages');
+        if (flashMessagesContainer) {
+            flashMessagesContainer.style.opacity = '0';
+            setTimeout(() => {
+                flashMessagesContainer.style.display = 'none';
+            }, 500); // Match the transition duration
+        }
+    }, 1500); // Adjust time as needed
+});
